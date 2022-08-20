dbf43a205c11c561fd96d4685106abfe0d8208c6
viamane <106557426+viadev228@users.noreply.github.com>
2022-08-20 09:45:51 +0300

    tech

---
+++ Password Generator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            int lenght = 8;
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < lenght--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            Console.WriteLine(res.ToString() + "\nEnter to regenerate password");
+            Console.ReadKey();
+            Console.Clear();
+            goto start;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Password Generator
- 
+Classic C# Password Generator.
+# About
+You can generate password. No logs, no spectating.
+
+# Where i can download it?
+Look at Release.
+# Authors 
+Creator - [FanyaOff](https://github.com/fanyaoff)
+
+# Have an issue?
+Contact me @vialcz on Telegram or open an issue.
+
+
+
+
+
